05d38a2d8f1eca094e3d299f6d3ab5ac4dbc7c10
Sébastien GAUTIER <sgautier034@gmail.com>
2023-09-07 10:47:32 +0200

    Add entity

---
+++ src/Entity/Projects.php
@@ -0,0 +1,8 @@
+<?php
+
+class Projects
+{
+    private $id_project;
+    private $title;
+    private $description;
+}
---
+++ src/Entity/Users.php
@@ -0,0 +1,10 @@
+<?php
+
+class Users
+{
+    private $id_user;
+    private $name;
+    private $firstname;
+    private $email;
+    private $password;
+}
