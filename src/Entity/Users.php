cd0ee8b35390a5b8983c6ef5806e958e6879840f
Sébastien GAUTIER <sgautier034@gmail.com>
2023-09-07 10:52:59 +0200

    Add method=> User.php

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -7,4 +7,54 @@ class Users
     private $firstname;
     private $email;
     private $password;
+
+    public function getId_user()
+    {
+        return $this->id_user;
+    }
+
+    public function setId_user($id_user)
+    {
+        return $this->id_user = $id_user;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        return $this->$name = $name;
+    }
+
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname($firstname)
+    {
+        return $this->$firstname = $firstname;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        return $this->$email = $email;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        return $this->$password = $password;
+    }
 }
