73cb6e92ba265109acec0e22b4fcfcd563cc5592
Sébastien GAUTIER <sgautier034@gmail.com>
2023-09-09 09:07:42 +0200

    Add Priority.php=> methods and Create project.html

--- src/Entity/Priority.php
+++ src/Entity/Priority.php
@@ -4,6 +4,28 @@ namespace Seb\GestionDeProjets\Entity;
 
 class Priority
 {
-    private int $idPriority;
-    private string $namePriority;
+    private int $id;
+    private string $name;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): Priority
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return ucfirst($this->name);
+    }
+
+    public function setName(string $name): Priority
+    {
+        $this->name = $name;
+        return $this;
+    }
 }
