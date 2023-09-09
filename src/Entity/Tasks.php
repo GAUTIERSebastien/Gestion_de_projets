b9d6df850bdc761b7cdb09ce041641acf80aa1d0
Sébastien GAUTIER <sgautier034@gmail.com>
2023-09-09 09:21:21 +0200

    Add Tasks.php=> methods

--- src/Entity/Tasks.php
+++ src/Entity/Tasks.php
@@ -4,7 +4,40 @@ namespace Seb\GestionDeProjets\Entity;
 
 class Tasks
 {
-    private int $idTask;
+    private int $id;
     private string $title;
     private string $description;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): Tasks
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getTitle(): string
+    {
+        return ucfirst($this->title);
+    }
+
+    public function setTitle(string $title): Tasks
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function getDescription(): string
+    {
+        return ucfirst($this->description);
+    }
+
+    public function setDescription(string $description): Tasks
+    {
+        $this->description = $description;
+        return $this;
+    }
 }
