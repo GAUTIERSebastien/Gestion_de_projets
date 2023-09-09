9f784b63e8b4aafaf0c08359310835d7747a4b5b
Sébastien GAUTIER <sgautier034@gmail.com>
2023-09-09 09:14:20 +0200

    Add Projects.php=> methods

--- src/Entity/Projects.php
+++ src/Entity/Projects.php
@@ -2,7 +2,40 @@
 
 class Projects
 {
-    private int $idProject;
+    private int $id;
     private string $title;
     private string $description;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): Projects
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getTitle(): string
+    {
+        return ucfirst($this->title);
+    }
+
+    public function setTitle(string $title): Projects
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function getDescription(): string
+    {
+        return ucfirst($this->description);
+    }
+
+    public function setDescription(string $description): Projects
+    {
+        $this->description = $description;
+        return $this;
+    }
 }
