63f199a5c8c7f55104d2d96be37063e498c2bf7f
Sébastien GAUTIER <sgautier034@gmail.com>
2023-09-09 09:16:06 +0200

    Add Status.php=> methods

--- src/Entity/Status.php
+++ src/Entity/Status.php
@@ -4,6 +4,28 @@ namespace Seb\GestionDeProjets\Entity;
 
 class Status
 {
-    private int $idStatus;
-    private string $nameStatus;
+    private int $id;
+    private string $name;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): Status
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return ucfirst($this->name);
+    }
+
+    public function setName(string $name): Status
+    {
+        $this->name = $name;
+        return $this;
+    }
 }
