5ba6ee8a312dc27b093859836a7af7cee90f4cc9
Sébastien GAUTIER <sgautier034@gmail.com>
2023-09-14 09:30:56 +0200

    Add comments

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
+// à mettre dans un autre fichier
 session_start();
 
 require('vendor/autoload.php');
