6fee69edb0235f6e8da35fd8f56e8787bdab7b02
Sébastien GAUTIER <sgautier034@gmail.com>
2023-09-15 16:04:34 +0200

    Update task ok

--- src/Controller/Task.php
+++ src/Controller/Task.php
@@ -29,4 +29,39 @@ class Task extends AbstractController
             header("Location: " . $_SERVER['HTTP_REFERER']);
         }
     }
+
+    public function update()
+    {
+        if (isset($_GET['id'])) {
+            $task = Tasks::getById($_GET['id']);
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $id = $_GET['id'];
+                $title = $_POST['title'];
+                $description = $_POST['description'];
+                $id_user = $task->getIdUser();
+                $id_status = $task->getIdStatus();
+                $id_priority = $task->getIdPriority();
+                $id_project = $task->getIdProject();
+
+                Tasks::update($id, [
+                    'title' => $title,
+                    'description' => $description,
+                    'id_user' => $id_user,
+                    'id_status' => $id_status,
+                    'id_priority' => $id_priority,
+                    'id_project' => $id_project
+                ]);
+                header("Location: index.php?controller=Project&method=showProject&id=$id_project");
+            }
+        }
+
+        $view = new Views();
+        $view->setHead('head.html');
+        $view->setHeader('header.html');
+        $view->setHtml('task.html');
+        $view->setFooter('footer.html');
+        $view->render([
+            'titlePage' => 'Task'
+        ]);
+    }
 }
