979e541a0d2c8711505e5e88dd7aaef5a5c53b9e
Sébastien GAUTIER <sgautier034@gmail.com>
2023-09-16 17:11:24 +0200

    Update style and Task=> create(not fonctional)

--- src/Controller/Task.php
+++ src/Controller/Task.php
@@ -5,6 +5,7 @@ namespace Seb\GestionDeProjets\Controller;
 use Seb\GestionDeProjets\Kernel\Views;
 use Seb\GestionDeProjets\Kernel\AbstractController;
 use Seb\GestionDeProjets\Entity\Tasks;
+use Seb\GestionDeProjets\Entity\Projects;
 
 
 class Task extends AbstractController
@@ -65,37 +66,36 @@ class Task extends AbstractController
             'task' => $task,
         ]);
     }
-    // public function create()
-    // {
-    //     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    //             $id = $_GET['id'];
-    //             $title = $_POST['title'];
-    //             $description = $_POST['description'];
-    //             $id_user = $task->getIdUser();
-    //             $id_status = $task->getIdStatus();
-    //             $id_priority = $task->getIdPriority();
-    //             $id_project = $task->getIdProject();
+    public function create()
+    {
+        if (isset($_GET['id'])) {
+            $id_project = Projects::getById($_GET['id']);
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $title = $_POST['title'];
+                $description = $_POST['description'];
+                $id_user = $_POST['id_user'];
+                $id_status = $_POST['id_status'];
+                $id_priority = $_POST['id_priority'];
+                $id_project = $_POST['id_project'];
 
-    //             Tasks::update($id, [
-    //                 'title' => $title,
-    //                 'description' => $description,
-    //                 'id_user' => $id_user,
-    //                 'id_status' => $id_status,
-    //                 'id_priority' => $id_priority,
-    //                 'id_project' => $id_project
-    //             ]);
-    //             header("Location: index.php?controller=Project&method=showProject&id=$id_project");
-    //         }
-    //     }
-
-    //     $view = new Views();
-    //     $view->setHead('head.html');
-    //     $view->setHeader('header.html');
-    //     $view->setHtml('task.html');
-    //     $view->setFooter('footer.html');
-    //     $view->render([
-    //         'titlePage' => 'Task'
-    //     ]);
-    // }
-
+                Tasks::insert([
+                    'title' => $title,
+                    'description' => $description,
+                    'id_user' => $id_user,
+                    'id_status' => $id_status,
+                    'id_priority' => $id_priority,
+                    'id_project' => $id_project
+                ]);
+            }
+        }
+        $view = new Views();
+        $view->setHead('head.html');
+        $view->setHeader('header.html');
+        $view->setHtml('createTask.html');
+        $view->setFooter('footer.html');
+        $view->render([
+            'titlePage' => 'Task'
+        ]);
+        header("Location: index.php?controller=Project&method=showProject&id=$id_project");
+    }
 }
